636ac998173e623d0c6351281b67d0293eaa14fe
Maribel Hearn <maribelhearn@disroot.org>
2025-09-23 23:05:53 +0200

    ??? Glitchy Terrain

--- js/pokedex.js
+++ js/pokedex.js
@@ -187,7 +187,7 @@ var PokedexTypePanel = PokedexResultPanel.extend({
 				atLeastOne = true;
 			}
 		}
-		if (type === '???' || type === 'unknown') {
+		if (type.name === '???') {
 			buf += '<div><small><a href="/moves/glitchyterrain" data-target="push">Glitchy Terrain</a> damage</small></div>';
 			atLeastOne = true;
 		}
